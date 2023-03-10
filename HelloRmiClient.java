94857827069aa458762e6a4bba18a2670b9b6647
Bart van der Wal <bart.vanderwal@han.nl>
2023-03-10 15:33:03 +0000

    Long method refactor main() +Exception toetsvraag.

--- HelloRmiClient.java
+++ HelloRmiClient.java
@@ -5,22 +5,38 @@ import java.rmi.registry.Registry;
 
 public class HelloRmiClient {
 
-    private HelloRmiClient() {}
+    // private HelloRmiClient() {}
 
-    public static void main(String[] args) {
+    private static HelloRmiInterface proxy;
+    
+    public static void main(String[] args) throws RemoteException {
+        // Default host voor RMI Registry, als leeg dan localhost op port 1099.
+        var host = (args.length < 1) ? null : args[0];
+        var helloClient = new HelloRmiClient();
 
-        String host = (args.length < 1) ? null : args[0];
+        proxy = lookupHelloProxy(host);
+        var response = helloClient.sayHello();
+        System.out.println("response: " + response);
+    }
+
+    private static HelloRmiInterface lookupHelloProxy(String host) {
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             HelloRmiInterface proxy = (HelloRmiInterface) registry.lookup(HelloRmiInterface.NAAM);
-            String response = proxy.sayHello();
-            System.out.println("response: " + response);
-        } catch (RemoteException e) {
+            return proxy;
+       } catch (RemoteException e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         } catch (NotBoundException e) {
             System.err.println(HelloRmiInterface.NAAM + "niet bekend in RMI Registry: " + e.toString());
             e.printStackTrace();
         }
+        return null;
+    }
+
+    // We gaan ervan uit dat als ophalen proxy goed ging, er dan geen verdere RemoteExceptions meer optreden.
+    // En vangen deze daarom niet op.
+    private String sayHello() throws RemoteException {
+        return proxy.sayHello();
     }
 }
